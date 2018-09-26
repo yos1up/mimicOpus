9dbe3d8a3a9f182b1b14a86984418dee08d12f57
masashi yoshikawa <yoshikawa@al4.biz>
2018-09-26 21:25:49 +0900

    [fix] NoteBlock linter

--- src/components/NoteBlock.jsx
+++ src/components/NoteBlock.jsx
@@ -1,30 +1,56 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 
-class NoteBlock extends React.Component { //ノーツ
+class NoteBlock extends React.Component { // ノーツ
   /*
     start: ノートオン時刻
     end: ノートオフ時刻
     pitch: ノートナンバー
     parent: 親 PianoRollGrid インスタンス．
   */
-  constructor(props){super(props);}
-  render(){
-    var start = this.props.start;
-    var end = this.props.end;
-    var pitch = this.props.pitch;
-    var pianoRollGrid = this.props.parent;
-    var divStyle = {width:pianoRollGrid.state.uw * (end - start), height:pianoRollGrid.state.uh, backgroundColor: 'blue',
-      borderRadius: 10, position:'absolute', top:0, left:0, opacity:0.5};
-    return(
-      <div style={divStyle} onMouseDown={this.click.bind(this)}></div>
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    const { delNote } = this.props;
+    // クリックイベントの親オブジェクトへの伝播を止める
+    event.stopPropagation();
+    delNote();
+  }
+
+  render() {
+    const {
+      start, end, parent: pianoRollGrid,
+    } = this.props;
+    const divStyle = {
+      width: pianoRollGrid.state.uw * (end - start),
+      height: pianoRollGrid.state.uh,
+      backgroundColor: 'blue',
+      borderRadius: 10,
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      opacity: 0.5,
+    };
+    return (
+      <div
+        style={divStyle}
+        onMouseDown={this.handleClick}
+        role="button"
+        tabIndex="0"
+      />
     );
   }
-  click(event){
-    //クリックイベントの親オブジェクトへの伝播を止める
-    event.stopPropagation();
-    this.props.delNote();
-  }
 }
+
+NoteBlock.propTypes = {
+  start: PropTypes.number.isRequired,
+  end: PropTypes.number.isRequired,
+  parent: PropTypes.object.isRequired,
+  delNote: PropTypes.func.isRequired,
+};
 
 export default NoteBlock;
