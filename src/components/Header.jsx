3e00a68d759bcf3a36745f1c76c3dea367eb94eb
masashi yoshikawa <yoshikawa@al4.biz>
2018-10-03 17:56:50 +0900

    [add] User avator menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,68 +5,129 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Avatar from '@material-ui/core/Avatar';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import ButtonBase from '@material-ui/core/ButtonBase';
+
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
 
 import displayModes from '../data/displayModes';
 
 
-function Header({
-  mode, uimage, changeDisplayMode, openSignInDialog,
-}) {
-  return (
-    <div id="Header">
-      <AppBar
-        style={{
-          position: 'fixed',
-          left: 0,
-          top: 0,
-          width: '100%',
-          height: 48,
-          zIndex: 10,
-          backgroundColor: '#ffffff',
-        }}
-      >
-        <div
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { openUserMenu: false };
+    this.handleOpenUserMenu = this.handleOpenUserMenu.bind(this);
+    this.handleCloseUserMenu = this.handleCloseUserMenu.bind(this);
+  }
+
+  handleOpenUserMenu() {
+    this.setState({ openUserMenu: true });
+  }
+
+  handleCloseUserMenu() {
+    this.setState({ openUserMenu: false });
+  }
+
+  render() {
+    const {
+      mode, uimage, changeDisplayMode, openSignInDialog,
+    } = this.props;
+    const { openUserMenu } = this.state;
+    return (
+      <div id="Header">
+        <AppBar
           style={{
-            position: 'relative',
-            width: 1000,
-            margin: '0px auto',
+            position: 'fixed',
+            left: 0,
             top: 0,
+            width: '100%',
+            height: 48,
+            zIndex: 10,
+            backgroundColor: '#ffffff',
           }}
         >
-          <Tabs
-            value={(mode === displayModes.SEARCH
-              || mode === displayModes.MAKE_QUESTION
-              || mode === displayModes.USER) ? mode : false}
-            onChange={(e, v) => changeDisplayMode(v)}
-            indicatorColor="primary"
-            textColor="primary"
-            style={{ float: 'left' }}
+          <div
+            style={{
+              position: 'relative',
+              width: 1000,
+              margin: '0px auto',
+              top: 0,
+            }}
           >
-            <Tab label="検索" value={displayModes.SEARCH} />
-            <Tab label="問題を作る" value={displayModes.MAKE_QUESTION} />
-            <Tab label="ユーザー" value={displayModes.USER} />
-          </Tabs>
-          {(uimage !== null && uimage !== undefined && uimage !== '')
-            ? (
-              <Avatar
-                alt="no image"
-                src={uimage}
-                style={{ float: 'left', left: 10, top: 5 }}
-                onClick={() => openSignInDialog()}
-              />
-            ) : (
-              <Avatar
-                style={{ float: 'left', left: 10, top: 5 }}
-                onClick={() => openSignInDialog()}
-              >
-                <AccountCircleIcon />
-              </Avatar>
-            )
-          }
-        </div>
-      </AppBar>
-    </div>
-  );
+            <Tabs
+              value={(mode === displayModes.SEARCH
+                || mode === displayModes.MAKE_QUESTION) ? mode : false}
+              onChange={(e, v) => changeDisplayMode(v)}
+              indicatorColor="primary"
+              textColor="primary"
+              style={{ float: 'left' }}
+            >
+              <Tab label="検索" value={displayModes.SEARCH} />
+              <Tab label="問題を作る" value={displayModes.MAKE_QUESTION} />
+            </Tabs>
+            {(uimage !== null && uimage !== undefined && uimage !== '')
+              ? (
+                <ButtonBase buttonRef={(node) => { this.anchorEl = node; }}>
+                  <Avatar
+                    alt="no image"
+                    src={uimage}
+                    style={{ float: 'left', left: 10, top: 5 }}
+                    onClick={() => this.handleOpenUserMenu()}
+                  />
+                </ButtonBase>
+              ) : (
+                <ButtonBase buttonRef={(node) => { this.anchorEl = node; }}>
+                  <Avatar
+                    style={{ float: 'left', left: 10, top: 5 }}
+                    ref={(node) => { this.anchorEl = node; }}
+                    onClick={() => this.handleOpenUserMenu()}
+                  >
+                    <AccountCircleIcon />
+                  </Avatar>
+                </ButtonBase>
+              )
+            }
+            <Popper open={openUserMenu} anchorEl={this.anchorEl} transition disablePortal>
+              {({ TransitionProps, placement }) => (
+                <Grow
+                  {...TransitionProps}
+                  id="menu-list-grow"
+                  style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+                >
+                  <Paper>
+                    <ClickAwayListener onClickAway={this.handleCloseUserMenu}>
+                      <MenuList>
+                        <MenuItem onClick={() => {
+                          this.handleCloseUserMenu();
+                          changeDisplayMode(displayModes.USER);
+                        }}
+                        >
+                          Profile
+                        </MenuItem>
+                        <MenuItem onClick={() => {
+                          this.handleCloseUserMenu();
+                          openSignInDialog();
+                        }}
+                        >
+                          Signin
+                        </MenuItem>
+                        <MenuItem onClick={this.handleCloseUserMenu}>Logout</MenuItem>
+                      </MenuList>
+                    </ClickAwayListener>
+                  </Paper>
+                </Grow>
+              )}
+            </Popper>
+          </div>
+        </AppBar>
+      </div>
+    );
+  }
 }
 
 Header.propTypes = {
