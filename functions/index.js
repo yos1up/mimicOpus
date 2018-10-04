51c8c343170c68676fbf09161bf24dc3897252eb
masashi yoshikawa <yoshikawa@al4.biz>
2018-10-04 19:12:59 +0900

    [add] functions questionList

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,21 @@
 const functions = require('firebase-functions');
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+const admin = require('firebase-admin');
+
+admin.initializeApp();
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send('Hello from Firebase!');
 });
+
+exports.questionsList = functions.https.onRequest((request, response) => {
+  const questionsRef = admin.firestore().collection('questions');
+  questionsRef.orderBy('uploadedAt', 'desc').limit(10).get().then(
+    (querySnapshot) => {
+      response.send(querySnapshot.docs.map(item => item.data()));
+      return 0;
+    }
+  ).catch(reason => {
+      res.send(reason)
+  });
+})
