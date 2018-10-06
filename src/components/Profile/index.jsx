c9608393f6e736f120322aac3b5f4f040540c119
masashi yoshikawa <yoshikawa@al4.biz>
2018-10-06 11:15:50 +0900

    [add] display username

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,15 +1,44 @@
 import firebase from 'firebase';
 import React from 'react';
 import PropTypes from 'prop-types';
+import Avatar from '@material-ui/core/Avatar';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import Typography from '@material-ui/core/Typography';
 
 function Profile(props) {
   const { user } = props;
+  const avatarStyle = {
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    width: 200,
+    height: 200,
+  };
   return (
     <div>
-      <img
-        alt="画像なし"
-        src={user.photoURL}
-      />
+      {(user.photoURL !== null && user.photoURL !== undefined && user.photoURL !== '')
+        ? (
+          <Avatar
+            alt="no image"
+            src={user.photoURL}
+            style={avatarStyle}
+          />
+        ) : (
+          <Avatar
+            style={avatarStyle}
+          >
+            <AccountCircleIcon />
+          </Avatar>
+        )
+      }
+      <Typography
+        variant="display1"
+        style={{
+          position: 'absolute', top: 210, left: 10, width: 180,
+        }}
+      >
+        {user.displayName}
+      </Typography>
     </div>
   );
 }
