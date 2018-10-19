166f7a29cabeea7cbf96eb22f696860c56922ca4
masashi yoshikawa <yoshikawa@al4.biz>
2018-10-19 20:13:44 +0900

    [fix] license display

--- src/client/components/License.js
+++ src/client/components/License.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Dialog from '@material-ui/core/Dialog';
-import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
 import Typography from '@material-ui/core/Typography';
 
@@ -9,10 +8,16 @@ import Typography from '@material-ui/core/Typography';
 function License({ open, closeLicenseDialog }) {
   return (
     <Dialog open={open} onClose={() => closeLicenseDialog()}>
-      <DialogTitle id="simple-dialog-title">ライセンス&クレジット</DialogTitle>
       <DialogContent>
-        <Typography>
-          yos1upとmarshiが頑張って開発したよ。
+        <Typography variant="h7">
+          This service is developed by
+        </Typography>
+        <Typography variant="h7">
+          Yuki Yoshida (yos1up) & Masashi Yoshikawa (marshi)
+        </Typography>
+        <br />
+        <Typography variant="h7">
+          This service is releaced under the MIT License.
         </Typography>
       </DialogContent>
     </Dialog>
