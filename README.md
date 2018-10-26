76666688c5fccea55737a1569f1a0fa24f7ac7d1
masashi yoshikawa <yoshikawa@al4.biz>
2018-10-27 07:24:49 +0900

    [change] sql and readme

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
-
 ## PostgreSQL Setup
 
 ```
--- mimicopus.sql
+++ mimicopus.sql
@@ -1,3 +1,10 @@
 CREATE TABLE questions (id SERIAL, notes JSON, bpm REAL, uid INTEGER, title TEXT, uploadedAt TIMESTAMP, rating REAL);
 CREATE TABLE scores (id SERIAL, qid INTEGER, uid INTEGER, score REAL);
 CREATE TABLE users (id SERIAL, provider TEXT, idByProvider TEXT, username TEXT, photoURL TEXT, totalscore INTEGER, rating REAL);
+CREATE TABLE "session" (
+  "sid" varchar NOT NULL COLLATE "default",
+        "sess" json NOT NULL,
+        "expire" timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
