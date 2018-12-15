3dd78d5137370e491499b69c33b36151170b16ac
masashi yoshikawa <yoshikawa@al4.biz>
2018-12-15 17:52:36 +0900

    [add] move evaluation logit to server

--- src/client/components/PlayQuestion/index.js
+++ src/client/components/PlayQuestion/index.js
@@ -28,84 +28,9 @@ const playModes = {
 
 
 class PlayQuestion extends React.Component {
-  static evaluateAnswer(qNotes, aNotes) {
-    /* 評価関数どうしますかね・・・
-
-    Wasserstein距離？ 編集距離？ 重なりの面積？ 完全一致？ F-value? 音ゲー式？
-    評価対象はノートオンのみ？
-
-    qMel, aMel: ノーツオブジェクトを各要素にもつ array
-                ノーツオブジェクト： {start:(ノートオン時刻[s]), end:(ノートオフ時刻[s]), pitch:(ノートナンバー)}
-    */
-    // 音ゲー式をまずは実装してみる．
-    const qEventOfEachPitch = Array(128);
-    for (let i = 0; i < 128; i += 1) qEventOfEachPitch[i] = new Array(0);
-    const aEventOfEachPitch = Array(128);
-    for (let i = 0; i < 128; i += 1) aEventOfEachPitch[i] = new Array(0);
-    for (let i = 0; i < qNotes.size; i += 1) {
-      qEventOfEachPitch[qNotes.get(i).pitch].push(qNotes.get(i).start);
-    }
-    for (let i = 0; i < aNotes.size; i += 1) {
-      aEventOfEachPitch[aNotes.get(i).pitch].push(aNotes.get(i).start);
-    }
-
-    const redundantPenalty = -0.5; // [pt]
-    const relevantInterval = 0.1; // [s]
-    const scoreDecay = 0.1; // [s]
-    let score = 0;
-
-    for (let i = 0; i < 128; i += 1) { // ピッチごとに採点をする．
-      if (qEventOfEachPitch[i].length === 0) {
-        score += redundantPenalty * aEventOfEachPitch[i].length;
-      } else {
-        qEventOfEachPitch[i].sort((a, b) => a - b);
-        aEventOfEachPitch[i].sort((a, b) => a - b);
-        /*  回答の各ノーツを，時刻の早いものから順に見ていく．
-            回答の各ノーツについて，
-              その回答ノーツに「近接」した正解ノーツがある場合は
-                それらの近接度に応じてスコアを加算し（同時なら満点），その正解ノーツを「削除」する．
-              その回答ノーツに「近接」した正解ノーツがない場合は，
-                スコアを減点する．
-        */
-        const check = Array(qEventOfEachPitch[i].length).fill(false);
-        let qOffset = 0;
-        for (let j = 0; j < aEventOfEachPitch[i].length; j += 1) {
-          const t = aEventOfEachPitch[i][j];
-          while (qOffset < qEventOfEachPitch[i].length
-            && qEventOfEachPitch[i][qOffset] < t) qOffset += 1;
-
-          let leftidx = qOffset - 1; // tの左側で，最も近接した「未チェックノート」との時間差
-          while (leftidx >= 0 && check[leftidx]) leftidx -= 1;
-          const left = (leftidx >= 0) ? (t - qEventOfEachPitch[i][leftidx]) : Infinity;
-
-          let rightidx = qOffset; // tの右側で，最も近接した「未チェックノート」との時間差
-          while (rightidx < qEventOfEachPitch[i].length
-            && check[rightidx]) rightidx += 1;
-          const right = (rightidx < qEventOfEachPitch[i].length)
-            ? (qEventOfEachPitch[i][rightidx] - t) : Infinity;
-
-          const minDist = Math.min(left, right);
-          // console.log(minDist);
-          if (minDist < relevantInterval) { // 「近接」した正解ノーツがある場合は
-            score += Math.exp(-minDist / scoreDecay);
-            check[(left < right) ? leftidx : rightidx] = true;
-          } else { // 「近接」した正解ノーツがない場合は，
-            score += redundantPenalty;
-          }
-        }
-      }
-    }
-    score = Math.max(0, score);
-    score *= 100 / qNotes.size;
-
-    return score;
-  }
-
   constructor(props) {
     super(props);
     this.state = {
-      dialogOpened: false,
-      dialogText: '',
       playMode: playModes.STOP,
       currentBeat: null,
       startBeat: 0,
@@ -121,10 +46,12 @@ class PlayQuestion extends React.Component {
   }
 
   componentDidMount() {
-    const { changeDisplayMode, clearNotes, loadBestSubmission, questionId } = this.props;
+    const {
+      changeDisplayMode, clearNotes, loadBestSubmission, questionId,
+    } = this.props;
     clearNotes();
 
-    /* 
+    /*
       過去に行った回答があるならば，それをセットしたい．
     */
     loadBestSubmission(questionId);
@@ -132,43 +59,13 @@ class PlayQuestion extends React.Component {
     changeDisplayMode(displayModes.PLAY_QUESTION);
   }
 
-  evaluateAndReport(qNotes, aNotes) {
-    const { questionId, saveAnswer } = this.props;
-    let score = PlayQuestion.evaluateAnswer(qNotes, aNotes);
-    if (score !== undefined && score !== null) {
-      score = parseFloat(score).toFixed(2);
-    } else {
-      score = 'Nothing';
-    }
-    let message = '';
-    message += `YOUR SCORE: ${score}\n`;
-    // if (score >= 100) message += '!!! CONGRATULATION !!!';
-
-    // スコアを表示．
-    this.handleOpenDialog(message);
-
-    // データベースとうろく
-    saveAnswer(questionId, aNotes, score);
-  }
-
-  handleCloseDialog() {
-    /* モーダルダイアログを閉じる */
-    this.setState({ dialogOpened: false });
-  }
-
-  handleOpenDialog(message) {
-    /* モーダルダイアログを開く */
-    this.setState({ dialogOpened: true, dialogText: message });
-  }
-
   render() {
     const {
       notes, question, pitchRange, bpm, addNote, delNote,
-      shiftPitchRange,
+      shiftPitchRange, questionId, submitAnswer, isOpenScoreDialog,
+      textScoreDialog, closeScoreDialog,
     } = this.props;
     const {
-      dialogOpened,
-      dialogText,
       playMode,
       currentBeat,
       startBeat,
@@ -238,9 +135,9 @@ class PlayQuestion extends React.Component {
             color="primary"
             aria-label="Submit"
             style={{ position: 'absolute', top: 10, left: 150 }}
-            onClick={() => this.evaluateAndReport(
-              question.notes, notes,
-            )}
+            onClick={() => {
+              submitAnswer(questionId, notes);
+            }}
           >
             <SendIcon />
           </Button>
@@ -267,15 +164,15 @@ class PlayQuestion extends React.Component {
         </div>
 
         <Dialog
-          open={dialogOpened}
+          open={isOpenScoreDialog}
           onClose={() => {
-            this.handleCloseDialog();
+            closeScoreDialog();
           }}
         >
           <DialogTitle id="simple-dialog-title">RESULT</DialogTitle>
           <DialogContent>
             <Typography>
-              {dialogText}
+              {textScoreDialog}
             </Typography>
           </DialogContent>
         </Dialog>
@@ -290,13 +187,16 @@ PlayQuestion.propTypes = {
   pitchRange: PropTypes.arrayOf(PropTypes.number).isRequired,
   questionId: PropTypes.string.isRequired,
   bpm: PropTypes.number.isRequired,
+  isOpenScoreDialog: PropTypes.bool.isRequired,
+  textScoreDialog: PropTypes.string.isRequired,
   shiftPitchRange: PropTypes.func.isRequired,
   addNote: PropTypes.func.isRequired,
   delNote: PropTypes.func.isRequired,
   clearNotes: PropTypes.func.isRequired,
-  saveAnswer: PropTypes.func.isRequired,
+  submitAnswer: PropTypes.func.isRequired,
   changeDisplayMode: PropTypes.func.isRequired,
   loadBestSubmission: PropTypes.func.isRequired,
+  closeScoreDialog: PropTypes.func.isRequired,
 };
 
 export default PlayQuestion;
--- src/server/api.js
+++ src/server/api.js
@@ -267,23 +267,118 @@ const changeDisplayName = (req, res) => {
   }
 };
 
-const saveAnswer = (req, res) => {
-  if (req.isAuthenticated()) {
-    const data = req.body;
-    const query = {
-      text: 'INSERT INTO answers(qid, uid, notes, score) VALUES($1, $2, $3, $4)',
-      values: [data.qid, req.user.id, JSON.stringify(data.notes), data.score],
-    };
+const evaluateAnswer = (qNotes, aNotes) => {
+  /* 評価関数どうしますかね・・・
 
-    client.query(query)
-      .then(() => res.send({ errState: 0 }))
-      .catch((e) => {
-        console.log(e);
-        res.send({ errState: 1 });
-      });
-  } else {
-    res.send({ errState: 1 });
+  Wasserstein距離？ 編集距離？ 重なりの面積？ 完全一致？ F-value? 音ゲー式？
+  評価対象はノートオンのみ？
+
+  qMel, aMel: ノーツオブジェクトを各要素にもつ array
+              ノーツオブジェクト： {start:(ノートオン時刻[s]), end:(ノートオフ時刻[s]), pitch:(ノートナンバー)}
+  */
+  // 音ゲー式をまずは実装してみる．
+  const qEventOfEachPitch = Array(128);
+  for (let i = 0; i < 128; i += 1) qEventOfEachPitch[i] = new Array(0);
+  const aEventOfEachPitch = Array(128);
+  for (let i = 0; i < 128; i += 1) aEventOfEachPitch[i] = new Array(0);
+  for (let i = 0; i < qNotes.length; i += 1) {
+    qEventOfEachPitch[qNotes[i].pitch].push(qNotes[i].start);
   }
+  for (let i = 0; i < aNotes.length; i += 1) {
+    aEventOfEachPitch[aNotes[i].pitch].push(aNotes[i].start);
+  }
+
+  const redundantPenalty = -0.5; // [pt]
+  const relevantInterval = 0.1; // [s]
+  const scoreDecay = 0.1; // [s]
+  let score = 0;
+
+  for (let i = 0; i < 128; i += 1) { // ピッチごとに採点をする．
+    if (qEventOfEachPitch[i].length === 0) {
+      score += redundantPenalty * aEventOfEachPitch[i].length;
+    } else {
+      qEventOfEachPitch[i].sort((a, b) => a - b);
+      aEventOfEachPitch[i].sort((a, b) => a - b);
+      /*  回答の各ノーツを，時刻の早いものから順に見ていく．
+          回答の各ノーツについて，
+            その回答ノーツに「近接」した正解ノーツがある場合は
+              それらの近接度に応じてスコアを加算し（同時なら満点），その正解ノーツを「削除」する．
+            その回答ノーツに「近接」した正解ノーツがない場合は，
+              スコアを減点する．
+      */
+      const check = Array(qEventOfEachPitch[i].length).fill(false);
+      let qOffset = 0;
+      for (let j = 0; j < aEventOfEachPitch[i].length; j += 1) {
+        const t = aEventOfEachPitch[i][j];
+        while (qOffset < qEventOfEachPitch[i].length
+          && qEventOfEachPitch[i][qOffset] < t) qOffset += 1;
+
+        let leftidx = qOffset - 1; // tの左側で，最も近接した「未チェックノート」との時間差
+        while (leftidx >= 0 && check[leftidx]) leftidx -= 1;
+        const left = (leftidx >= 0) ? (t - qEventOfEachPitch[i][leftidx]) : Infinity;
+
+        let rightidx = qOffset; // tの右側で，最も近接した「未チェックノート」との時間差
+        while (rightidx < qEventOfEachPitch[i].length
+          && check[rightidx]) rightidx += 1;
+        const right = (rightidx < qEventOfEachPitch[i].length)
+          ? (qEventOfEachPitch[i][rightidx] - t) : Infinity;
+
+        const minDist = Math.min(left, right);
+        // console.log(minDist);
+        if (minDist < relevantInterval) { // 「近接」した正解ノーツがある場合は
+          score += Math.exp(-minDist / scoreDecay);
+          check[(left < right) ? leftidx : rightidx] = true;
+        } else { // 「近接」した正解ノーツがない場合は，
+          score += redundantPenalty;
+        }
+      }
+    }
+  }
+  score = Math.max(0, score);
+  score *= 100 / qNotes.length;
+
+  return score;
+};
+
+const submitAnswer = (req, res) => {
+  const data = req.body;
+  let query = {
+    text: 'SELECT * from questions WHERE id = $1',
+    values: [data.qid],
+  };
+
+  client.query(query)
+    .then((result) => {
+      const qNotes = result.rows[0].notes;
+      const score = evaluateAnswer(qNotes, data.notes);
+
+      if (req.isAuthenticated()) {
+        query = {
+          text: 'INSERT INTO answers(qid, uid, notes, score) VALUES($1, $2, $3, $4)',
+          values: [data.qid, req.user.id, JSON.stringify(data.notes), score],
+        };
+
+        client.query(query)
+          .then(() => res.send({
+            score,
+            saved: true,
+            errState: 0,
+          }))
+          .catch((e) => {
+            console.log(e);
+            res.send({ errState: 1 });
+          });
+      } else {
+        res.send({
+          score,
+          saved: false,
+          errState: 0,
+        });
+      }
+    }).catch((e) => {
+      console.log(e);
+      res.send({ errState: 1 });
+    });
 };
 
 const getRanking = (req, res) => {
@@ -360,7 +455,7 @@ apiRouter.post('/api/uploadQuestion', uploadQuestion);
 apiRouter.get('/api/loadQuestionsList', loadQuestionsList);
 apiRouter.get('/api/countQuestions', countQuestions);
 apiRouter.post('/api/deleteQuestion', deleteQuestion);
-apiRouter.post('/api/saveAnswer', saveAnswer);
+apiRouter.post('/api/submitAnswer', submitAnswer);
 apiRouter.post('/api/changeDisplayName', changeDisplayName);
 apiRouter.get('/api/getRanking', getRanking);
 apiRouter.get('/api/getMe', getMe);
